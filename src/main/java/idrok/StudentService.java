3f5cd49f66e83f96681892dd0bcd3e32e4b638c8
Jumaniyoz5112 <5112jumaniyoz@gmail.com>
2025-11-08 16:02:04 +0500

    add StudentService and spring configuration for application context

---
+++ src/main/java/idrok/StudentService.java
@@ -0,0 +1,13 @@
+package idrok;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentService {
+    public void todo() {
+        System.out.println("Student Service TODO.");
+
+    }
+}
